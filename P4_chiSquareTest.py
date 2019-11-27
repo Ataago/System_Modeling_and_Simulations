00f8abc5f7fd3a85f30449cc3871fe3b27954a65
Mohammed Ataaur Rahaman <ataago7@gmail.com>
2019-11-27 22:19:44 +0530

    v2.0

--- P4_chiSquareTest.py
+++ P4_chiSquareTest.py
@@ -2,8 +2,8 @@
 #   P4_chiSquareTest.py
 #   Chi Square Test
 #
-#   Created by Sanjitha Singh on 2/11/19.
-#   Copyright © 2019 SanjithaSingh. All rights reserved.
+#   Created by Mohammed Ataa on 26/11/19.
+#   Copyright © 2019 Ataago. All rights reserved.
 # 
 #   Write a program to show goodness of fit test using Chi-Square Test 
 #   for the input set of random numbers. Assume significance value is equal to 0.05. 
@@ -12,61 +12,45 @@
 
 from random import random
 
+# Random Digits
+RD = [round(random(), 2) for i in range(int(input("Enter total number of random numbers to be generatted: ")))]
+# RD = [0.67, 0.34, 0.90, 0.25, 0.89, 0.74, 0.83, 0.76, 0.79, 0.64, 0.02, 0.96, 0.99, 0.77, 0.67, 0.05, 0.47, 0.30, 0.17, 0.82, 0.42, 0.79, 0.71, 0.23, 0.19]
+print("Random Digits:\n", RD)
+# Total Class Intervals n
+n = int(input("Enter Total Class intervals: "))
 
-def input_random_numbers():
-    random_numbers = input("Enter the random numbers: ")
-    random_numbers = [float(i) for i in random_numbers.split()]
-    return random_numbers
-
-
-def generate_random_numbers(n): #optional
-    random_numbers = [round(random(), 4) for i in
-                      range(n)]  # n random numbers generated between 0 to 1 rounded to 4 digits
-    print("Generated Random numbers:", random_numbers)
-    return random_numbers
-
-
-random_numbers = input_random_numbers()
-# random_numbers=generate_random_numbers(20)
-n = int(input("Enter the number of class intervals: "))
-
-# generating upper-bound of ranges
+# Range of Classes for n as class interval
+interval = [1/n] * n
 ranges = []
-interval = 1 / n
-for i in range(1, n + 1):
-    ranges.append(interval * i)
+for i in range(n):
+    ranges.append(round(sum(interval[0:i + 1]),1))
 
-# calculating frequencies for observed values (Oi)
-frequency = [0] * n
-for i in random_numbers:
-    for j in range(n):
-        if i <= ranges[j]:
-            frequency[j] += 1
+# Observed Frequency = Oi (frequency in the ranges)
+Oi = [0] * n
+for num in RD:
+    for i in range(n):
+        if num <= ranges[i]:
+            Oi[i] += 1
             break
 
-# Calculating expected value Ei
-N = len(random_numbers)
-expected_value = N / n
+# Eperimental values = Ei = N/n
+Ei = len(RD)/n
 
-# Calculating ((Oi-Ei)^2)/Ei
-summation = 0
-summation_array = []
-for o in frequency:
-    result = ((o - expected_value) ** 2) / expected_value
-    summation += result
-    summation_array.append(result)
-
-# display the calculation table
-print("Class(n)\tOi\tEi\t(Oi-Ei)^2/Ei")
-print("-" * 30)
+# Chi Square X^2 = (Oi - Ei)^2 / Ei
+X = []
 for i in range(n):
-    print("\t{}\t\t{}\t{}\t{}".format((i + 1), frequency[i], expected_value, summation_array[i]))
+    X.append(((Oi[i] - Ei) ** 2) / Ei)
 
-print("Summation: ", summation)
+# Printing the Table
+print("\nClass\tRange\tOi\tEi\tX^2")
+print('-' * 35)
+for i in range(n):
+    print("{}\t{}\t{}\t{}\t{}" .format(i + 1, ranges[i], Oi[i], Ei, X[i]))
 
-# Check if the null hypothesis is accepted or not
-dcritical = float(input("Enter the D-Critical Value: "))
-if summation < dcritical:
-    print("Null hypothesis is accepted")
+print('\nChi Square: ', sum(X))
+Dcritical = float(input("Enter D Critical Value: "))
+
+if sum(X) <= Dcritical:
+    print('Hypothesis H0 is Accepted')
 else:
-    print("Null Hypothesis is rejected")
+    print('Hypothesis H0 is Rejected')
