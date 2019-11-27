36c80265b646818cf6a4e2ba1a3eaa39f50f23a6
Mohammed Ataaur Rahaman <ataago7@gmail.com>
2019-11-27 23:33:55 +0530

    module updated
    removed math module and simplifed the operations

--- P5_autoCorrelationTest.py
+++ P5_autoCorrelationTest.py
@@ -9,19 +9,18 @@
 #   Assume Zα/2=1.96
 #
 
-from math import floor, sqrt
 from random import random
 
 # Generaing the random Digits for testing he goodness of fit (Independance property)
 R = [round(random(), 2) for i in range(int(input("Enter Total number of random digis to be generated: ")))]
-R = [
-    0.12, 0.01, 0.23, 0.28, 0.89, 
-    0.31, 0.64, 0.28, 0.83, 0.93, 
-    0.99, 0.15, 0.33, 0.35, 0.91, 
-    0.41, 0.60, 0.27, 0.75, 0.88, 
-    0.68, 0.49, 0.05, 0.43, 0.95, 
-    0.58, 0.19, 0.36, 0.69, 0.87
-]
+# R = [
+#     0.12, 0.01, 0.23, 0.28, 0.89, 
+#     0.31, 0.64, 0.28, 0.83, 0.93, 
+#     0.99, 0.15, 0.33, 0.35, 0.91, 
+#     0.41, 0.60, 0.27, 0.75, 0.88, 
+#     0.68, 0.49, 0.05, 0.43, 0.95, 
+#     0.58, 0.19, 0.36, 0.69, 0.87
+# ]
 
 # Inputing Starting point, i & gap, m
 i = int(input("Enter starting point to test Independance Property: i = "))
